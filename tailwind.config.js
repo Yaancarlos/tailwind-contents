53d33e241daca842cc07e95bb7c842da0871c891
Yan Carlos <urbanoyaan@gmail.com>
2022-06-08 22:40:43 -0500

    Files are added

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    fontFamily: {
+      'sans': ['DM Sans'],
+    },
+    extend: {
+      colors: {
+        'violet-page': '#1E0E62',
+        'violetish': '#2F1893',
+      },
+    },
+  },
+  plugins: [],
+}
